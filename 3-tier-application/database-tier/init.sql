2344b658ac2c2887b1c3af768006c6e2fe49c2f3
PavanThumati <pavanthumati999@gmail.com>
2025-06-16 14:32:53 +0530

    Create init.sql

---
+++ 3-tier-application/database-tier/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE userdb;
+
+USE userdb;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO users (name) VALUES ('Alice'), ('Bob');
